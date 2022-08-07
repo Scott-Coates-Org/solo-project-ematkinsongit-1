3900f550587631e2377533a2929aab58f8f10fb1
ematkinsongit <90931047+ematkinsongit@users.noreply.github.com>
2022-08-07 11:31:00 -1000

    added routing for games pages. resolved #28 and resolved #8

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import Counter from "./components/Counter";
 import logo from "./logo.svg";
 import "./App.css";
 import Login from "./components/login/Login";
+import { Routes, Route, Router } from "react-router-dom";
+import DoVerbGame from "./pages/DoVerbGame";
+import Home from "./pages/Home";
+import NounGame from "./pages/NounGame";
+import PhraseGame from "./pages/PhraseGame";
+import VerbMatchGame from "./pages/VerbMatchGame";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Counter />
-      </header>
-      <Login />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/verbmatchgame" element={<VerbMatchGame />} />
+      <Route path="/noungame" element={<NounGame />} />
+      <Route path="/phrasegame" element={<PhraseGame />} />
+      <Route path="/doverbgame" element={<DoVerbGame />} />
+    </Routes>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './redux/store'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</React.StrictMode>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
