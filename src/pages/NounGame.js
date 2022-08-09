bdd75be8871611a55e8a926f3cb3f268ebf2423a
ematkinsongit <90931047+ematkinsongit@users.noreply.github.com>
2022-08-09 11:52:57 -1000

    added some functionality to nounGame. still need to work out some bugs

--- src/pages/NounGame.js
+++ src/pages/NounGame.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "reactstrap";
+import "./NounGame.css";
+import ButtonList from "../components/ButtonList";
+import SingleButton from "../components/login/SingleButton";
 
 const NounGame = () => {
-  return <div>NounGame</div>;
+  const [style, setStyle] = useState("reg");
+  const [word, setWord] = useState("");
+  const fillerSentence = [
+    { word: "One", isAnswer: false },
+    { word: "blue", isAnswer: false },
+    { word: "bird", isAnswer: true },
+    { word: "sits", isAnswer: false },
+    { word: "on", isAnswer: false },
+    { word: "a", isAnswer: false },
+    { word: "branch", isAnswer: true },
+  ];
+  const readAloud = () => {};
+  const buttonList = fillerSentence.map((pair) => {
+    return (
+      <button
+        className={style}
+        key={pair.word}
+        value={pair.word}
+        onMouseEnter={readAloud(pair.word)}
+        onClick={() => handleClick(pair.isAnswer)}
+      >
+        {pair.word}
+      </button>
+    );
+  });
+
+  const handleClick = (isAnswer) => {
+    if (isAnswer === true) {
+      setStyle("isAnswer");
+      console.log("correct!");
+      //unfortunately sets style of EVERY button. Needs to be only the current button. fix later.
+    } else {
+      console.log("sorry, incorrect");
+      setStyle("reg");
+    }
+  };
+
+  return <div className="main">{buttonList}</div>;
 };
 
 export default NounGame;
